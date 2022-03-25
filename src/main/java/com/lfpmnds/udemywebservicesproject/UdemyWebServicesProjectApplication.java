d07ec0f10df2d50ca0f51a97a19b381e46cb353a
Luiz Felipe <luizfelipe.m93@gmail.com>
2022-03-25 09:31:26 -0300

    Projeto criado

---
+++ src/main/java/com/lfpmnds/udemywebservicesproject/UdemyWebServicesProjectApplication.java
@@ -0,0 +1,13 @@
+package com.lfpmnds.udemywebservicesproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UdemyWebServicesProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UdemyWebServicesProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/lfpmnds/udemywebservicesproject/UdemyWebServicesProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lfpmnds.udemywebservicesproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UdemyWebServicesProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
