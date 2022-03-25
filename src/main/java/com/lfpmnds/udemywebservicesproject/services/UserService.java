7453edc92bbbba6907b5f28efa37626c4145a394
Luiz Felipe <luizfelipe.m93@gmail.com>
2022-03-25 15:37:07 -0300

    Camada de serviço e registro de componentes

---
+++ src/main/java/com/lfpmnds/udemywebservicesproject/services/UserService.java
@@ -0,0 +1,26 @@
+package com.lfpmnds.udemywebservicesproject.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.lfpmnds.udemywebservicesproject.entities.User;
+import com.lfpmnds.udemywebservicesproject.repositories.UserRepository;
+
+@Service
+public class UserService {
+
+	@Autowired
+	private UserRepository repository;
+	
+	public List<User> findAll() {
+		return repository.findAll();
+	}
+	
+	public User findById(Long id) {
+		Optional<User> obj = repository.findById(id);
+		return obj.get();
+	}
+}
