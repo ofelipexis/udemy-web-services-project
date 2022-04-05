b869d53c4ca1d6b7d3b1b2cff3e760c3064ba26e
Luiz Felipe <luizfelipe.m93@gmail.com>
2022-04-05 12:04:00 -0300

    Tratamento de exceção do findById

--- src/main/java/com/lfpmnds/udemywebservicesproject/services/UserService.java
+++ src/main/java/com/lfpmnds/udemywebservicesproject/services/UserService.java
@@ -8,6 +8,7 @@ import org.springframework.stereotype.Service;
 
 import com.lfpmnds.udemywebservicesproject.entities.User;
 import com.lfpmnds.udemywebservicesproject.repositories.UserRepository;
+import com.lfpmnds.udemywebservicesproject.services.exceptions.ResourceNotFoundException;
 
 @Service
 public class UserService {
@@ -21,7 +22,7 @@ public class UserService {
 	
 	public User findById(Long id) {
 		Optional<User> obj = repository.findById(id);
-		return obj.get();
+		return obj.orElseThrow(() -> new ResourceNotFoundException(id));
 	}
 	
 	public User insert(User obj) {
